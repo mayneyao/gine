a33280f2e65e969d7ebd3ba3e207b5d581965bd3
Mayne <i@gine.me>
2022-12-02 03:11:18 +0800

    chore: refactor package

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading() {
   return <div className="flex items-center justify-center h-screen">
-    <div className="w-10 h-10 border-2 border-t-2 border-gray-900 rounded-full animate-spin"></div>
+    <div className="w-10 h-10 border-2 border-t-2 rounded-full animate-spin"></div>
   </div>
 }
