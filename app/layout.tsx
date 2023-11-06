19a8d157b71ef7e4bd00b6d6ba82140c650202a6
Mayne <i@gine.me>
2023-11-06 15:49:23 +0800

    Add Umami analytics script to head.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import './globals.css';
-import { ThemeProvider } from 'next-themes'
-
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -15,13 +14,16 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-
+      <head>
+        <script
+          async
+          src="https://analytics.eu.umami.is/script.js"
+          data-website-id="ce7a8343-7766-433d-84f8-9e69a4dcd0fd"
+        ></script>
+      </head>
       <body>
-        <ThemeProvider attribute="class">
-          {children}
-        </ThemeProvider>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
